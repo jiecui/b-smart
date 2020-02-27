83807816ad9fb13869c70470f9008e8178d0b36d
Richard Jie Cui <richard.cui@utoronto.ca>
2020-02-27 13:52:09 -0600

    change functions related Makefile.move

--- c_code/Makefile.move
+++ c_code/Makefile.move
@@ -1,8 +1,8 @@
 opssmov : opssmov.o MARfit.o EEGrealA.o MARgetpfpb.o matmisc.o matmisc2.o matmisc5.o EEG_tpool.o
 	cc -lm -o opssmov opssmov.o MARfit.o EEGrealA.o MARgetpfpb.o matmisc.o matmisc2.o matmisc5.o EEG_tpool.o
-opssmov.o : opssmov.c EEGdef.h EEGmat.h
+opssmov.o : opssmov.c EEGdef.h EEGmat.h MARdef.h
 	cc -c opssmov.c
-MARfit.o : MARfit.c EEGdef.h EEGmat.h
+MARfit.o : MARfit.c EEGdef.h EEGmat.h MARdef.h
 	cc -c MARfit.c
 EEGrealA.o : EEGrealA.c EEGdef.h EEGmat.h
 	cc -c EEGrealA.c
@@ -10,10 +10,9 @@ MARgetpfpb.o : MARgetpfpb.c EEGdef.h EEGmat.h
 	cc -c MARgetpfpb.c
 matmisc.o : matmisc.c
 	cc -c matmisc.c
-matmisc2.o : matmisc2.c
+matmisc2.o : matmisc2.c EEGmat.h
 	cc -c matmisc2.c
 matmisc5.o : matmisc5.c
 	cc -c matmisc5.c
 EEG_tpool.o : EEG_tpool.c
 	cc -c EEG_tpool.c
-
--- c_code/opssmov.c
+++ c_code/opssmov.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include "EEGdef.h"
 #include "EEGmat.h"
+#include "MARdef.h"
 
-
-main(int argc, char**argv)
+int main(int argc, char**argv)
 { int NPTS =0;
   int NCHN =0;
   int NTRLS =0;
@@ -94,7 +94,7 @@ else {
 
 
   if((inpt = fopen(argv[1],"rb")) == NULL) {
-	printf("\007Error opening input file!\n"); return;
+	printf("\007Error opening input file!\n"); return 0;
   } 
 /*  if((shfp = fopen(argv[2],"rt")) == NULL) {
 	printf("\007Error opening shift file!\n"); return;
@@ -170,7 +170,7 @@ else {
 	given time instant  ***/
 
   if((fp = fopen(argv[2],"a")) == NULL) {
-	printf("\007Error opening MAR coeff file!\n"); return;
+	printf("\007Error opening MAR coeff file!\n"); return 0;
   } 
    for ( i=0; i < MODORDER+1; i++) /* 5 order, 1st is identity; if 7, then new =0 */
 	 for ( j=0; j < NCHN*NCHN; j++)
@@ -179,7 +179,7 @@ else {
    fclose(fp);
    
   if((fp = fopen(argv[3],"a")) == NULL) {
-	printf("\007Error opening MAR noise file!\n"); return;
+	printf("\007Error opening MAR noise file!\n"); return 0;
   } 
    for ( i=0; i < NCHN*NCHN; i++)
 	 fprintf(fp,"%.3g  ",Ve[i]);
