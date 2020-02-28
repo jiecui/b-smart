679ff36edbe5131c8fb50fbcc805f7ec41457acf
Richard Jie Cui <richard.cui@utoronto.ca>
2020-02-28 17:15:51 -0600

    update pre_sube.m to R2019b

--- functions/pre_sube.m
+++ functions/pre_sube.m
@@ -4,21 +4,24 @@ function data = pre_sube(dat)
 % Usage:
 %  data = pre_sube(dat);
 % 
-%  dat: input file;
+%  dat: input data in MAT format = samples x channels x trials
 %  data: output file after subtract the ensemble mean
 
 % Copyright (c) 2006-2007 BSMART group.
 % by Richard Cui
-% $Revision: 0.2$ $Date: 14-Sep-2007 15:35:25$
-% SHIS UT-Houston, Houston, TX 77030, USA.
+% $Revision: 0.3$ $Date: Fri 02/28/2020  4:47:19.718 PM $
 %
+% 1026 Rocky Creek Dr NE
+% Rochester, MN 55906, USA
+%
+% Email: richard.cui@utoronto.ca
 
-[points channel trail] = size(dat);
-for i = 1:channel
-    b = dat(:,i,:);
-    c = mean(b,3);      % channel ensemble mean
-    for j = 1:trail
-        dat(:,i,j) = dat(:,i,j)-c;  % subtract esnemble mean
+[num_chan, num_trl] = size(dat, [2, 3]);
+for i = 1:num_chan
+    b_i = dat(:,i,:);
+    c_i = mean(b_i,3);      % channel ensemble mean
+    for j = 1:num_trl
+        dat(:,i,j) = dat(:,i,j)-c_i;  % subtract esnemble mean
     end
 end
 data = dat;
