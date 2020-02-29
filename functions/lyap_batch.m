391c82105d3b5a8e465c84f5a6ca8be49f505dbd
Richard Jie Cui <richard.cui@utoronto.ca>
2020-02-29 16:38:24 -0600

    update lyap_batch.m

--- functions/lyap_batch.m
+++ functions/lyap_batch.m
@@ -4,31 +4,37 @@ function LE = lyap_batch(arcoeff,arnoise,T)
 % Usage: 
 %   LE = lyap_batch(arcoeff,arnoise,T);
 % 
-%   T   -   Number of samples to generate;
+%   arcoeff         - model coefficients
+%   arnoise         - noise coefficients
+%   T   -   Number of samples to generate
+% 
+%   LE              - Lyponov exponent ( < 0 not stable)
 % 
 % Note: 
 %   In fact we do not use ARNOISE in Lyapunov calculation, 
 %   but need it in MAR_make routine
 %
+% See also .
 
-% Copyright (c) 2006-2007 BSMART group.
+% Copyright (c) 2006-2020 BSMART group.
 % by Richard Cui
-% $Revision: 0.2$ $Date: 12-Sep-2007 10:39:05$
-% SHIS UT-Houston, Houston, TX 77030, USA.
+% $Revision: 0.3$ $Date: Sat 02/29/2020  2:50:39.405 PM $
 %
-% Hualou Liang, 02/09/99, FAU
-% 
+% 1026 Rocky Creek Dr NE
+% Rochester, MN 55906, USA
+%
+% Email: richard.cui@utoronto.ca
 
 LE = [];
 len = size(arnoise,1);
-h = waitbar(0,'Please wait...');    % TODO: set a window title
-for t = 1:len,
-    waitbar(t/len);
+fw = waitbar(0,'Please wait...', 'Name', 'Lyapnov Exponent');
+for t = 1:len
+    waitbar(t/len, fw);
     mar = MAR_make(arcoeff(t,:), arnoise(t,:));
     le = lyap(mar,T,1000);          % LE = lyap(mar, T, discardnum);
     LE = cat(2,LE,le);
-end;
-close(h);
+end
+delete(fw);
 
 end%lyap_batch
 
